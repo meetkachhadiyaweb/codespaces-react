5a75d0270520a0e7311e08261e162ac5d35c5a0b
meetkachhadiyaweb <kachhadiyameet140@gmail.com>
2024-07-29 05:50:05 +0000

    I added H2

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,10 @@
 function Home(){
     return(
-        <h1>Welcome to Home Page</h1>
+        <>
+         <h1>Welcome to Home Page</h1>
+         <h2>This is H2 tag</h2>
+        </>
+       
     )
 }
 export default Home
